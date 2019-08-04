c11d94d4595908fe85444d230863a79e374575ac
integrarium <integrarium@gmail.com>
2019-08-04 22:09:11 +0500

    Create lang.php

---
+++ lang/en/lang.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * English language file for codebuttontrumod plugin
+ *
+ * @author Alexey Trushnikov <integrarium@gmail.com>
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+// custom language strings for the plugin
+// $lang['fixme'] = 'FIXME';
+
+$lang['insertcode'] = 'Insert Code';
